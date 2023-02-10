0c2885e6d09c4de9dcbfffe7197ac80249743fcb
ychunx <ychunx@qq.com>
2023-02-10 23:36:31 +0800

    上传聊天图片接口

--- router/modules/uploadFile.js
+++ router/modules/uploadFile.js
@@ -2,12 +2,13 @@
 const formidable = require('formidable')
 const fs = require('fs')
 
+const address = 'http://localhost:3000'
 const mkdirs = require('../../dao/mkdirs')
 
 module.exports = (router) => {
     // 上传头像
     router.post('/upload/portrait', (req, res) => {
-        const form = formidable({ });
+        const form = formidable({ })
 
         form.parse(req, (err, fields, files) => {
             let { mimetype, newFilename, originalFilename, filepath } = files.portraitFile
@@ -21,7 +22,35 @@ module.exports = (router) => {
                     } else {
                         try {
                             fs.copyFileSync(filepath, toPath)
-                            res.cc(`http://localhost:3000/portraitImages/${newFilename}.${fileType}`, 200)
+                            res.cc(`${address}/portraitImages/${newFilename}.${fileType}`, 200)
+                        } catch (error) {
+                            res.cc(error)
+                        }
+                    }
+                })
+            } else {
+                res.cc('上传文件非图片', 201)
+            }
+        })
+    })
+
+    // 上传聊天图片
+    router.post('/upload/image', (req, res) => {
+        const form = formidable({ })
+
+        form.parse(req, (err, fields, files) => {
+            let { mimetype, newFilename, originalFilename, filepath } = files.uploadImgFile
+            if (mimetype.indexOf('image') != '-1') {
+                let fileType = originalFilename.replace(/.+\./, '')
+                let toPath = `./public/msgImages/${newFilename}.${fileType}`
+
+                mkdirs.mkdir('../public/msgImages', (err) => {
+                    if (err) {
+                        res.cc(err)
+                    } else {
+                        try {
+                            fs.copyFileSync(filepath, toPath)
+                            res.cc(`${address}/msgImages/${newFilename}.${fileType}`, 200)
                         } catch (error) {
                             res.cc(error)
                         }
